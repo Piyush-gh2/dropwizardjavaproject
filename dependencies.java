d7177e11663c9ada5c776e60a25205de9156d93f
Piyush-gh2 <117152866+Piyush-gh2@users.noreply.github.com>
2024-03-02 08:20:22 +0530

    this is my dropwizardjavaproject

---
+++ dependencies.java
@@ -0,0 +1,11 @@
+<properties>
+<dropwizard.version>LATEST VERSION</dropwizard.version>
+</properties>
+Then list the dropwizard-core library:
+<dependencies>
+<dependency>
+<groupId>io.dropwizard</groupId>
+<artifactId>dropwizard-core</artifactId>
+<version>${version}</version>
+</dependency>
+</dependencies>
